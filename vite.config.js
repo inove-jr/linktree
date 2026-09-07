748d0f737e299d69c20295ba2909f501150a9acc
Eduardo dos Santos <perfilprofissional.eduardo@gmail.com>
2026-09-07 16:44:07 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/linktree/', // Substitua pelo nome exato do repositório no GitHub
 })
